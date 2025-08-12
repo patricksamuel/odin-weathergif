c64831df6801b9f2ac96a14abae2f64a5d544e2e
Patrick Samuel <10101460+patricksamuel@users.noreply.github.com>
2025-08-12 14:10:50 +0200

    giphy function added

--- src/index.js
+++ src/index.js
@@ -4,19 +4,37 @@ console.log("test js")
 
 
 async function fetchWeatherApiAsync(searchTerm = 'rouen') {
-    const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${searchTerm}?unitGroup=us&key=FDCWNZ8N7997VKBEFJLLDZXYM&contentType=json`, { mode: "cors" });
+    const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${encodeURIComponent(searchTerm)}?unitGroup=us&key=FDCWNZ8N7997VKBEFJLLDZXYM&contentType=json`, { mode: "cors" });
     const dataApi = await response.json();
     return dataApi;
 }
 
-async function getvalue(){
-    const value = await fetchWeatherApiAsync('rouen');
+async function getgif(searchTerm = 'sunny') {
+    const response = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=lFYBk5P3EiVHuqWQdGsFKb504e7WUU11&s=${encodeURIComponent(searchTerm)}`, {mode: 'cors'});
+    const gifData = await response.json();
+    return gifData;
+  }
+
+
+
+async function run(){
+    let searchTerm = 'miami'
+    const value = await fetchWeatherApiAsync(searchTerm);
+    const image = await getgif(value.currentConditions.conditions)
     console.log(value)
     console.log(value.address)
     console.log(value.currentConditions.temp)
+    console.log(value.currentConditions.conditions)
     console.log(value.description);
+    console.log(image.data.images.original.url)
+    const mainContent = document.createElement("div")
+
+    const imagePlaceDom = document.createElement("img")
+    imagePlaceDom.src = image.data.images.original.url
+    document.body.appendChild(mainContent)
+    mainContent.appendChild(imagePlaceDom)
     
 }
-getvalue()
+run()
 
 
