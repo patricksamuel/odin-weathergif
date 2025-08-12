fc06548806b3fd7842235c63e9a51deca42f64d0
Patrick Samuel <10101460+patricksamuel@users.noreply.github.com>
2025-08-12 13:38:17 +0200

    api fetching work on console log

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 import "./styles.css"
 
 console.log("test js")
+
+
+async function fetchWeatherApiAsync(searchTerm = 'rouen') {
+    const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${searchTerm}?unitGroup=us&key=FDCWNZ8N7997VKBEFJLLDZXYM&contentType=json`, { mode: "cors" });
+    const dataApi = await response.json();
+    return dataApi;
+}
+
+async function getvalue(){
+    const value = await fetchWeatherApiAsync('rouen');
+    console.log(value)
+    console.log(value.address)
+    console.log(value.currentConditions.temp)
+    console.log(value.description);
+    
+}
+getvalue()
+
+
