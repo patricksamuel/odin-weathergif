61c61c7f3cb4af429de1f21278a75aaf27b0d583
Patrick Samuel <10101460+patricksamuel@users.noreply.github.com>
2025-08-12 17:42:23 +0200

    basic front end done suing template that I DIYd

--- src/index.js
+++ src/index.js
@@ -10,30 +10,48 @@ async function fetchWeatherApiAsync(searchTerm = 'rouen') {
 }
 
 async function getgif(searchTerm = 'sunny') {
+    //const response = await fetch(`https://api.giphy.com/v2/emoji?api_key=lFYBk5P3EiVHuqWQdGsFKb504e7WUU11&s=${encodeURIComponent(searchTerm)}`, {mode: 'cors'});
     const response = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=lFYBk5P3EiVHuqWQdGsFKb504e7WUU11&s=${encodeURIComponent(searchTerm)}`, {mode: 'cors'});
+
     const gifData = await response.json();
     return gifData;
   }
 
 
 
-async function run(){
-    let searchTerm = 'miami'
-    const value = await fetchWeatherApiAsync(searchTerm);
-    const image = await getgif(value.currentConditions.conditions)
-    console.log(value)
-    console.log(value.address)
-    console.log(value.currentConditions.temp)
-    console.log(value.currentConditions.conditions)
-    console.log(value.description);
-    console.log(image.data.images.original.url)
-    const mainContent = document.createElement("div")
+function run(){
+    let searchTerm = ''
 
-    const imagePlaceDom = document.createElement("img")
-    imagePlaceDom.src = image.data.images.original.url
-    document.body.appendChild(mainContent)
-    mainContent.appendChild(imagePlaceDom)
+    const mainContent = document.querySelector("#heroContent")
+    const generatedContent = document.createElement("div");
+    mainContent.appendChild(generatedContent);
+    const mainTitle = document.querySelector("#mainTitle")
     
+    const inputDom = document.querySelector("#inputDom")
+    const searchButton = document.querySelector("#searchButton")
+    searchButton.addEventListener("click",async (e) => {
+        e.preventDefault()
+        generatedContent.textContent='';
+        console.log("search utton clicked");
+        searchTerm = inputDom.value;
+        console.log(searchTerm)
+        const value = await fetchWeatherApiAsync(searchTerm);
+        const image = await getgif(value.currentConditions.conditions);
+        console.log(value)
+        console.log(value.address)
+        console.log(value.currentConditions.temp)
+        console.log(value.currentConditions.conditions)
+        console.log(value.description);
+        console.log(image.data.images.original.url)
+
+        const imagePlaceDom = document.createElement("img")
+        imagePlaceDom.src = image.data.images.original.url;
+        generatedContent.appendChild(imagePlaceDom);
+        mainTitle.textContent = " It is "+value.currentConditions.conditions+" in "+value.address;
+
+    })
+    console.log("end of function");
+
 }
 run()
 
